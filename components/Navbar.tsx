d700d9c7fe3db0f80fcc29538cf6a1d92a1b5ee7
Bhatiyaamit <129496121+Bhatiyaamit@users.noreply.github.com>
2026-03-20 15:11:25 +0530

    feature by copilot

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,45 @@
 import Link from "next/link";
 import Image from "next/image";
+import GlassSurface from "@/components/GlassSurface";
 
 const Navbar = () => {
     return (
-        <header>
+      <header>
+        <div className="px-24 pt-7">
+          <GlassSurface
+            width="100%"
+            height="auto"
+            borderRadius={999}
+            borderWidth={0.05}
+            brightness={70}
+            opacity={0.65}
+            blur={9}
+            saturation={1.25}
+            distortionScale={-140}
+            displace={0.35}
+          >
             <nav>
-                <Link href='/' className="logo">
-                    <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+              <Link href="/" className="logo">
+                <Image
+                  src="/icons/logo.png"
+                  alt="logo"
+                  width={24}
+                  height={24}
+                />
 
-                    <p>DevEvent</p>
-                </Link>
+                <p>DevEvent</p>
+              </Link>
 
-                <ul>
-                    <Link href="/">Home</Link>
-                    <Link href="/">Events</Link>
-                    <Link href="/">Create Event</Link>
-                </ul>
+              <ul>
+                <Link href="/">Home</Link>
+                <Link href="/">Events</Link>
+                <Link href="/">Create Event</Link>
+              </ul>
             </nav>
-        </header>
-    )
+          </GlassSurface>
+        </div>
+      </header>
+    );
 }
 
 export default Navbar
